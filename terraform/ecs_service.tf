e7d7c9eb5f91ad7b03ff8bea85101fbe5e5695f4
SVS <v.stepanets@kernel.ua>
2021-12-27 20:32:23 +0200

    Test network_conf

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -8,8 +8,8 @@
   deployment_maximum_percent         = 200
 
   network_configuration {
-    security_groups  = toset(aws_security_group.allow_traffic)
-    subnets          = toset(data.aws_subnet.public_subnets.*.id)
+    security_groups  = aws_security_group.allow_traffic
+    subnets          = data.aws_subnet.public_subnets.*.id
     assign_public_ip = true
   }
 }
